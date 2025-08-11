8ef2d54ae145f1e41602e1c64a1f9602b6da4f9c
saulmartinx <122527980+saulmartinx@users.noreply.github.com>
2025-08-11 14:17:10 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,16 +1,17 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
-	"log"
-	"net/http"
-	"os"
+    "database/sql"
+    "fmt"
+    "log"
+    "net/http"
+    "os"
+    "strconv"
 
-	"github.com/bugsnag/bugsnag-go"
-	"github.com/gorilla/sessions"
-	"github.com/kelseyhightower/envconfig"
-	_ "github.com/lib/pq"
+    "github.com/bugsnag/bugsnag-go"
+    "github.com/gorilla/sessions"
+    "github.com/kelseyhightower/envconfig"
+    _ "github.com/lib/pq"
 )
 
 var store *sessions.CookieStore
@@ -20,44 +21,57 @@ const sessionName = "superwork"
 var db *sql.DB
 
 func main() {
-	// Parse env variables
-	if err := envconfig.Process("superwork", &config); err != nil {
-		log.Fatal(err)
-	}
+    // Parse environment variables into config
+    if err := envconfig.Process("superwork", &config); err != nil {
+        log.Fatal(err)
+    }
 
-	bugsnag.Configure(bugsnag.Configuration{
-		APIKey: config.BugsnagAPIKey,
-	})
+    // If running on a platform like Render with a provided PORT, use it
+    if portStr := os.Getenv("PORT"); portStr != "" {
+        if p, err := strconv.Atoi(portStr); err == nil {
+            config.Port = p
+        } else {
+            log.Printf("WARNING: Invalid PORT value %q, using default port %d", portStr, config.Port)
+        }
+    }
 
-	// Setup cookie store
-	store = sessions.NewCookieStore([]byte(config.Secret))
+    // Configure Bugsnag for error monitoring
+    bugsnag.Configure(bugsnag.Configuration{
+        APIKey: config.BugsnagAPIKey,
+    })
 
-	// Validate oauth2 config
-	if len(config.GoogleRedirect) == 0 {
-		log.Panic("missing google oauth redirect url")
-	}
-	if len(config.FacebookRedirect) == 0 {
-		log.Panic("missing facebook oauth redirect url")
-	}
+    // Set up session cookie store
+    store = sessions.NewCookieStore([]byte(config.Secret))
 
-	// Open log file
-	f, err := os.OpenFile(config.Logfile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	if err != nil {
-		log.Fatalf("error opening file: %v", err)
-	}
-	defer f.Close()
-	if config.Log {
-		log.SetOutput(f)
-	}
+    // Validate OAuth2 redirect URLs (ensure they are set)
+    if len(config.GoogleRedirect) == 0 {
+        log.Panic("missing Google OAuth redirect URL")
+    }
+    if len(config.FacebookRedirect) == 0 {
+        log.Panic("missing Facebook OAuth redirect URL")
+    }
 
-	if err := connectDB("superwork"); err != nil {
-		log.Panic(err)
-	}
+    // Set up logging to file if enabled
+    if config.Log {
+        f, err := os.OpenFile(config.Logfile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+        if err != nil {
+            log.Fatalf("error opening log file: %v", err)
+        }
+        defer f.Close()
+        log.SetOutput(f)
+    }
 
-	r := defineRoutes()
+    // Connect to the database (using DATABASE_URL or default local settings)
+    if err := connectDB("superwork"); err != nil {
+        log.Panic(err)
+    }
 
-	log.Println("App started on http://localhost:" + fmt.Sprintf("%d", config.Port))
+    // Define all routes for the HTTP server
+    r := defineRoutes()
 
-	// Serve HTTP
-	log.Panic(http.ListenAndServe(fmt.Sprintf(":%d", config.Port), bugsnag.Handler(r)))
+    // Log the startup info
+    log.Printf("App started on port %d", config.Port)
+
+    // Start the HTTP server with Bugsnag error reporting
+    log.Panic(http.ListenAndServe(fmt.Sprintf(":%d", config.Port), bugsnag.Handler(r)))
 }
