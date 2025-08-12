0abd3b05b1599edc7c397f91f3bea6cbbe65e4d7
saulmartinx <122527980+saulmartinx@users.noreply.github.com>
2025-08-12 14:53:30 +0300

    Update oauth.go

--- oauth.go
+++ oauth.go
@@ -1,32 +1,30 @@
 package main
 
 import (
-	"os"
-
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/facebook"
 	"golang.org/x/oauth2/google"
 )
 
+func facebookOauthConf() *oauth2.Config {
+	return &oauth2.Config{
+		ClientID:     "222679544825854",
+		ClientSecret: "c5560b28c59b7d5afdec1b4d07e51a02",
+		RedirectURL:  config.FacebookRedirect,
+		Scopes:       []string{"email"},
+		Endpoint:     facebook.Endpoint,
+	}
+}
+
 func googleOauthConf() *oauth2.Config {
 	return &oauth2.Config{
-		ClientID:     os.Getenv("SUPERWORK_GOOGLE_CLIENT_ID"),
-		ClientSecret: os.Getenv("SUPERWORK_GOOGLE_CLIENT_SECRET"),
-		RedirectURL:  os.Getenv("SUPERWORK_GOOGLE_REDIRECT"),
+		ClientID:     "116806776326-1l7bm6htqeof2ftl72339j7d49jped0q.apps.googleusercontent.com",
+		ClientSecret: "uMqr7xxUcgY3MJ7_4Ns8YLvB",
+		RedirectURL:  config.GoogleRedirect,
 		Scopes: []string{
 			"https://www.googleapis.com/auth/userinfo.profile",
 			"https://www.googleapis.com/auth/userinfo.email",
 		},
 		Endpoint: google.Endpoint,
-	}
-}
-
-func facebookOauthConf() *oauth2.Config {
-	return &oauth2.Config{
-		ClientID:     os.Getenv("SUPERWORK_FACEBOOK_CLIENT_ID"),
-		ClientSecret: os.Getenv("SUPERWORK_FACEBOOK_CLIENT_SECRET"),
-		RedirectURL:  os.Getenv("SUPERWORK_FACEBOOK_REDIRECT"),
-		Scopes:       []string{"email"},
-		Endpoint:     facebook.Endpoint,
 	}
 }
